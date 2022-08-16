be00ac1f6085d5613fb19e105bb2969dcc5e43fa
psspl trainee63 <psspl.trainee63@outlook.com>
2022-08-16 17:17:42 +0530

    highlight row simple way

--- ToDoApp/src/app/Directives/even-odd-highlight.directive.ts
+++ ToDoApp/src/app/Directives/even-odd-highlight.directive.ts
@@ -4,32 +4,13 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appEvenOddHighlight]'
 })
 export class EvenOddHighlightDirective {
-  @Input() appEvenOddHighlight = "";
-  @Input() id !: string;
-
   
   constructor(private el: ElementRef) {
     
-    //this.el.nativeElement.style.backgroundColor = this.appEvenOddHighlight;
-    var test = this.appEvenOddHighlight;
+    this.el.nativeElement.style.backgroundColor = "yellow";
   }
-
-  // @HostListener('mouseenter') onMouseEnter() {
-  //   this.highlight(this.appEvenOddHighlight);
-  // }
-
-  // @HostListener('mouseleave') onMouseLeave() {
-  //   this.highlight('');
-  // }
-
-  // private highlight(color: string) {
-  //   this.el.nativeElement.style.backgroundColor = color;
-  // }
    
   ngOnInit(): void {
-    this.el.nativeElement.style.backgroundColor = "yellow";
-    
-    var test = this.id;
   }
 
 }
