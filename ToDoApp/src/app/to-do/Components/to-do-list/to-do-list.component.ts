4a1243cc060d6d0768aa583c33f0f04d683f347f
psspl trainee63 <psspl.trainee63@outlook.com>
2022-08-16 14:30:11 +0530

    filter all page

--- ToDoApp/src/app/to-do/Components/to-do-list/to-do-list.component.ts
+++ ToDoApp/src/app/to-do/Components/to-do-list/to-do-list.component.ts
@@ -1,33 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ToDoList } from 'src/app/Model/toDoList.Model';
+import { FilterPipe } from 'src/app/Pipes/filter.pipe';
 import { FetchToDoDataService } from 'src/app/Services/fetch-to-do-data.service';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
-  styleUrls: ['./to-do-list.component.scss']
+  styleUrls: ['./to-do-list.component.scss'],
+  providers: [FilterPipe]
 })
 export class ToDoListComponent implements OnInit {
   searchTerm:string = "";
   highlightAction: string = "None";
   p: number = 1;
   ToDoDetails: ToDoList[] = [];
-  constructor(private fetchToDoDataService: FetchToDoDataService) { }
+  ToDoDetailsAll: ToDoList[] = [];
+  constructor(private fetchToDoDataService: FetchToDoDataService, private filter: FilterPipe) { }
   
   ngOnInit(): void {
     this.fetchToDoDataService.getData().subscribe((data:any) => {
       this.ToDoDetails =data;
+      this.ToDoDetailsAll = data;
       //console.log(data);
     })
   }
 
-  // applyFilter(startWith: string) {
-  //   startWith = startWith.trim(); // Remove whitespace
-  //   startWith = startWith.toLowerCase(); // Datasource defaults to lowercase matches
-
-  //   this.ToDoDetails = this.ToDoDetails.filter((val: any) => val.todo.toLowerCase().startsWith(startWith) || val.id.toLowerCase().startsWith(startWith) || val.assignDate.toLowerCase().startsWith(startWith));
+  applyFilter(startWith: string) {
+    startWith = startWith.trim(); // Remove whitespace
+    startWith = startWith.toLowerCase(); // Datasource defaults to lowercase matches
     
-  //   this.p = 1;
-  // }
+    this.ToDoDetails = this.filter.transform(this.ToDoDetailsAll, startWith);
+    // this.ToDoDetails = this.ToDoDetails.filter((val: any) => val.todo.toLowerCase().startsWith(startWith) || val.id.toLowerCase().startsWith(startWith) || val.assignDate.toLowerCase().startsWith(startWith));
+    
+    this.p = 1;
+  }
 
 }
